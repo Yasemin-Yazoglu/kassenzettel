5f7cb9a664c8c039d9963fb1d2bb698c57dc3370
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-02-17 15:37:31 +0100

    Create CardsContainer

---
+++ app/_components/CardsContainer.tsx
@@ -0,0 +1,27 @@
+import { group_spendings } from "../utility/function";
+import { DateKey, Spending } from "../utility/type";
+
+type Props = {
+    type: DateKey;
+    spendings: Spending[];
+    onSelect: (year: number) => void;
+}
+
+export default function CardsContainer({
+    type,
+    spendings,
+    onSelect,
+}: Props) {
+    const sorted = group_spendings(type, spendings, "desc");
+
+    return (
+        <div className="container cards">
+            {sorted.map(([year, total]) => (
+                <button title={`${year} hast du ${total}€ ausgegeben`} className="glass card" onClick={() => onSelect(year)} key={year}>
+                    <h1>{year}</h1>
+                    <p className="balance">{total}&euro;</p>
+                </button>
+            ))}
+        </div>
+    );
+}
