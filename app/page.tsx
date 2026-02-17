48f80ba92f945ba150b097c19e70329458e207c8
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-02-17 15:21:50 +0100

    Add Components and set/get spendings - Contents:     * Added components: Logo, InputConainer, MonthsView, CardsContainer, Footer     * Get spendings from localStorage     * Set spendings to localStorage     * track spendings in a "Spending[]" array     * use a boolean to determine if the user want to view the months view     * store the selected year, of which the user want to see the detailed months view, in react state

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,65 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import CardsContainer from "./_components/CardsContainer";
+import InputContainer from "./_components/InputContainer";
 import Logo from "./_components/logo";
+import MonthsView from "./_components/MonthsView";
+import Footer from "./_components/Footer";
+import { Spending } from "./utility/type";
+import { Date_Enum } from "./utility/enum";
 
 export default function Home() {
+  const [spendings, setSpendings] = useState<Spending[]>([]);
+  const [monthView, setMonthView] = useState<boolean>(false);
+  const [selectedYear, setSelectedYear] = useState<number>(0);
+
+  useEffect(() => {
+    const data = localStorage.getItem("spendings");
+    if(data) {
+      setSpendings(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(
+      "spendings",
+      JSON.stringify(spendings)
+    );
+  }, [spendings]);
+
+  const handleSelection = (year: number) => {
+    setMonthView(true);
+    setSelectedYear(year);
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+    <div className="flex items-center justify-center">
+      <main className="flex flex-col items-center justify-between w-full gap-16">
         {/* Logo container */}
-        <div className="flex flex-row w-full items-center justify-around">
-          <Logo size={100} />
+        <div className="mx-auto pt-32">
+          <Logo />
+        </div>
+
+        {/* Input container */}
+        <div className="mx-auto">
+          <InputContainer setSpendings={setSpendings} />
+        </div>
+
+        {/* Cards Container */}
+        {monthView ? (
+          <div className="w-full">
+            <MonthsView selected_year={selectedYear} onClose={() => setMonthView(false)} spending={spendings.filter((spending) => spending.year === selectedYear)} />
+          </div>
+        ) : (
+          <div className="mx-auto w-full">
+            <CardsContainer type={Date_Enum.YEAR} spendings={spendings} onSelect={(year: number) => handleSelection(year)} />
+          </div>
+        )}
+
+        {/* Footer Section */}
+        <div className="mx-auto pb-8">
+          <Footer />
         </div>
       </main>
     </div>
