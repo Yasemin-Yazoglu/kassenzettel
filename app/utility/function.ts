f74d4602c0956da671cdc94e7771c43388bdf547
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-02-17 15:26:49 +0100

    Create file for functions - Contents:     * group_spendings: The user inputs the type of the date, the spendings list, and the sorting order. The function then groups the spending array by date type, and calculates for each group the total spending.     * month_to_string: Converts month number to month string.     * translate_date_to_german: Translates an english date type (year, month, day) to german.

---
+++ app/utility/function.ts
@@ -0,0 +1,69 @@
+import { DateKey, Sort, Spending } from "./type";
+
+export function group_spendings(type: DateKey, spendings: Spending[], order_by: Sort): [number, number][] {
+    const grouped = new Map<number, number>();
+
+    for(const entry of spendings) {
+        const key = entry[type] as number;
+
+        grouped.set(
+            key,
+            (grouped.get(key) || 0) + entry.amount
+        );
+    }
+
+    if(order_by === "desc") {
+        return [...grouped.entries()].sort(
+            (a, b) => b[0] - a[0]
+        );
+    }
+    else {
+        return [...grouped.entries()].sort(
+            (a, b) => a[0] - b[0]
+        );
+    }
+};
+
+export function month_to_string(month: number): string {
+    switch(month) {
+        case 1:
+            return "Januar";
+        case 2:
+            return "Februar";
+        case 3:
+            return "März";
+        case 4:
+            return "April";
+        case 5:
+            return "Mai";
+        case 6:
+            return "Juni";
+        case 7:
+            return "Juli";
+        case 8:
+            return "August";
+        case 9:
+            return "September";
+        case 10:
+            return "Oktober";
+        case 11:
+            return "November";
+        case 12:
+            return "Dezember";
+        default:
+            throw new Error("Ungültige Eingabe");
+    }
+};
+
+export function translate_date_to_german(date_type: DateKey): string {
+    switch (date_type) {
+        case "year":
+            return "Jahr";
+        case "month":
+            return "Monat";
+        case "day":
+            return "Tag";
+        default:
+            throw new Error("Ungültige Eingabe");
+    }
+};
