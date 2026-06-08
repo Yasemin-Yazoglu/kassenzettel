7687bb3ea61e8827263f80b0c25fed9e1f035247
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-06-08 23:45:30 +0200

    add formState Use formState to display UI based on if the user wants to login or signup. Default is login.

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,8 +3,9 @@
 import { useState } from "react";
 import Logo from '@/public/logo/kassenzettel.svg';
 import Link from "next/link";
+import { login } from "./actions";
 
-interface LoginForm {
+export interface LoginForm {
     email: string;
     password: string;
 }
@@ -16,7 +17,13 @@ interface SignUpForm {
 }
 
 export default function LoginPage() {
+    const [formState, setFormState] = useState<'login' | 'signup'>('login')
     const [visible, setVisible] = useState(false);
+    const [loginData, setLoginData] = useState<LoginForm>({
+        email: '',
+        password: '',
+    });
+
     // TODO: Solve reusability of Logo
     const LogoComponent = Logo;
 
@@ -24,6 +31,7 @@ export default function LoginPage() {
         <div className="min-h-screen flex items-center justify-center p-6">
             <div className="glass w-full max-w-md rounded-3xl p-8">
                 <div className="mt-6 text-center">
+                    {/* TODO: Remove Link */}
                     <Link href="/">
                         <LogoComponent className="w-64 mx-auto" />
                     </Link>
@@ -32,86 +40,95 @@ export default function LoginPage() {
                     Verfolge und analysiere Deine Ausgaben
                 </p>
     
-                <form className="space-y-5">
-                <div>
-                <label className="block text-sm text-slate-300 mb-2">
-                    Email
-                </label>    
-                <input
-                    type="email"
-                    placeholder="your@email.com"
-                    className="authInput"
-                />
-                </div>
-                <div>
-                    <label className="block text-sm text-slate-300 mb-2">
-                        Passwort
-                    </label>
-                    <div className="relative">
+                {formState === 'login' && (
+                    <form action={login} className="space-y-5">
+                    <div>
+                        <label className="block text-sm text-slate-300 mb-2">
+                            Email
+                        </label>    
                         <input
-                            id="password"
-                            name="password"
-                            type={visible ? "text" : "password"}
-                            required
-                            minLength={8}
-                            maxLength={72}
-                            autoComplete="current-password"
-                            placeholder="••••••••"
+                            type="email"
+                            name="email"
+                            placeholder="deine@email.com"
                             className="authInput"
                         />
+                    </div>
+                    <div>
+                        <label className="block text-sm text-slate-300 mb-2">
+                            Passwort
+                        </label>
+                        <div className="relative inputAutofillWrapper">
+                            <input
+                                id="password"
+                                name="password"
+                                type={visible ? "text" : "password"}
+                                required
+                                minLength={8}
+                                maxLength={72}
+                                autoComplete="current-password"
+                                placeholder="••••••••"
+                                className="authInput"
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setVisible((v) => !v)}
+                                aria-label={visible ? "Passwort verbergen" : "Passwort anzeigen"}
+                                className="absolute right-3 top-1/2 -translate-y-1/2
+                                        text-white/50 hover:text-white/80 transition"
+                            >
+                                {visible ? (
+                                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
+                                    <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94" />
+                                    <path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19" />
+                                    <line x1="1" y1="1" x2="23" y2="23" />
+                                </svg>
+                                ) : (
+                                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
+                                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                                    <circle cx="12" cy="12" r="3" />
+                                </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
+        
+                    {/*
+                    // TODO: Integrate this later
+                    <div className="flex items-center justify-end text-sm">
+                        
+                        <label className="flex items-center gap-2 text-slate-300">
+                            <input type="checkbox" />
+                            An mich erinnern
+                        </label>
+                        
+            
                         <button
                             type="button"
-                            onClick={() => setVisible((v) => !v)}
-                            aria-label={visible ? "Passwort verbergen" : "Passwort anzeigen"}
-                            className="absolute right-3 top-1/2 -translate-y-1/2
-                                    text-white/50 hover:text-white/80 transition"
+                            className="text-indigo-400 hover:text-indigo-300"
                         >
-                            {visible ? (
-                            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-                                <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94" />
-                                <path d="M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19" />
-                                <line x1="1" y1="1" x2="23" y2="23" />
-                            </svg>
-                            ) : (
-                            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
-                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                                <circle cx="12" cy="12" r="3" />
-                            </svg>
-                            )}
+                            Passwort vergessen?
                         </button>
                     </div>
-                </div>
-    
-                <div className="flex items-center justify-between text-sm">
-                    <label className="flex items-center gap-2 text-slate-300">
-                        <input type="checkbox" />
-                        An mich erinnern
-                    </label>
+                    */}
         
                     <button
-                        type="button"
-                        className="text-indigo-400 hover:text-indigo-300"
+                        type="submit"
+                        className="
+                            mt-4
+                            w-full
+                            py-3
+                            rounded-xl
+                            font-semibold
+                            text-white
+                            bg-indigo-600
+                            hover:bg-indigo-500
+                            transition
+                        "
                     >
-                        Passwort vergessen?
+                    Login
                     </button>
-                </div>
-    
-                <button
-                    type="submit"
-                    className="
-                        w-full
-                        py-3
-                        rounded-xl
-                        font-semibold
-                        text-white
-                        bg-indigo-600
-                        hover:bg-indigo-500
-                        transition
-                    "
-                >
-                Login
-                </button>
-            </form>
+                </form>
+            )}
     
             <div className="relative my-6">
                 <hr className="w-full border-t border-white/10" />
