6abbceef3c4ebb7c208dab821ebbcad668f52dd6
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-06-09 21:46:54 +0200

    Add email and password input fields

--- app/auth/login/components/LoginForm.tsx
+++ app/auth/login/components/LoginForm.tsx
@@ -10,6 +10,13 @@ export function LoginForm() {
     const [state, formAction, pending] = useActionState(login, initialState)
     return (
         <form action={formAction}>
+            {/* Email field */}
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+            
+            {/* Password field */}
+            <label htmlFor="password">Passwort</label>
+            <input type="password" id="password" name="password" />
             <SubmitButton />
         </form>
     );
