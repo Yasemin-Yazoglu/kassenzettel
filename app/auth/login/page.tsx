92af41dc3e122ffc6343a42f8c0edefe69b3f5e7
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-06-13 16:53:31 +0200

    remove old code and use component - The login with google button is replaced by a button component

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Logo from '@/public/logo/kassenzettel.svg';
 import Link from "next/link";
 import { LoginForm } from "./components/LoginForm";
+import { LoginWithGoogle } from "./components/LoginWithGoogle";
 
 export default function LoginPage() {
     const [formState, setFormState] = useState<'login' | 'signup'>('login')
@@ -52,44 +53,8 @@ export default function LoginPage() {
             <div className="relative my-6">
                 <hr className="w-full border-t border-white/10" />
             </div>
-    
-            <button
-                className="
-                w-full
-                flex
-                items-center
-                justify-center
-                gap-3
-                py-3
-                rounded-xl
-                border
-                border-white/10
-                bg-white/10
-                hover:bg-white/15
-                text-white
-                transition
-                "
-            >
-            <svg width="20" height="20" viewBox="0 0 48 48">
-                <path
-                    fill="#FFC107"
-                    d="M43.611 20.083H42V20H24v8h11.303C33.655 32.657 29.205 36 24 36c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.957 3.043l5.657-5.657C34.046 6.053 29.29 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.651-.389-3.917z"
-                />
-                <path
-                    fill="#FF3D00"
-                    d="M6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.957 3.043l5.657-5.657C34.046 6.053 29.29 4 24 4c-7.682 0-14.347 4.337-17.694 10.691z"
-                />
-                <path
-                    fill="#4CAF50"
-                    d="M24 44c5.188 0 9.86-1.977 13.409-5.192l-6.19-5.238C29.147 35.091 26.683 36 24 36c-5.184 0-9.626-3.329-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z"
-                />
-                <path
-                    fill="#1976D2"
-                    d="M43.611 20.083H42V20H24v8h11.303a12.046 12.046 0 01-4.084 5.57h.003l6.19 5.238C36.971 38.482 44 33 44 24c0-1.341-.138-2.651-.389-3.917z"
-                />
-            </svg>
-                Mit Google fortfahren
-            </button>
+            
+            <LoginWithGoogle />
                 <p className="text-center text-slate-400 text-sm mt-8">
                     Du hast noch keinen Account?{" "}
                     <button className="text-indigo-400 hover:text-indigo-300">
