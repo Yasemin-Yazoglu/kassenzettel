870319d96a0ed1cc8181d404654a4bf3535e2d27
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-06-28 17:57:20 +0200

    remove comments

--- app/utility/function.ts
+++ app/utility/function.ts
@@ -2,11 +2,9 @@ import { DateKey, Sort, Spending } from "./type";
 
 export function group_spendings(type: DateKey, spendings: Spending[], order_by: Sort): [number, number][] {
     const grouped = new Map<number, number>();
-    console.log("Spendings", spendings);
 
     for(const entry of spendings) {
         const key = entry[type] as number;
-        console.log(`Grouped key ${key}: `, grouped.get(key));
 
         grouped.set(
             key,
