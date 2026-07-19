726af85d27a981c09b6f66ba011dcc66831595cf
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-07-20 00:50:42 +0200

    use password schema

--- lib/validation/auth.ts
+++ lib/validation/auth.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { passwordSchema } from "./password";
 
 export const loginSchema = z
   .object({
@@ -6,11 +7,7 @@ export const loginSchema = z
       .email("Bitte gebe eine gültige email Adresse ein")
       .toLowerCase(),
 
-    password: z
-      .string()
-      .min(8, "Das Passwort muss mindestens 8 Zeichen lang sein")
-      .regex(/[A-Z]/, "Es muss einen Großbuchstaben enthalten")
-      .regex(/[a-z]/, "Es muss einen Kleinbuchstaben enthalten")
-  })
+    password: passwordSchema,
+  });
 
 export type LoginInput = z.infer<typeof loginSchema>;
