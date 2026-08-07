6fb7aefa3bbaaa35df959f39c536f6ed4bef8cef
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-08-07 23:22:11 +0200

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import { getLanguage } from "@/public/locales";
 import Navbar from "./_components/Navbar";
@@ -29,6 +30,7 @@ export default function RootLayout({
           <Footer />
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
