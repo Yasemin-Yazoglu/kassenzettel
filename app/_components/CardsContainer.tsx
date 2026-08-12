29df8d00f1423919015f78652f17f90c8c9d72ed
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-08-12 23:15:10 +0200

    replace custom css with tailwind

--- app/_components/CardsContainer.tsx
+++ app/_components/CardsContainer.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "@/lib/formatCurrency";
 import { group_spendings } from "../utility/function";
 import { DateKey, Spending } from "../utility/type";
 
@@ -15,11 +16,18 @@ export default function CardsContainer({
     const sorted = group_spendings(type, spendings, "desc");
 
     return (
-        <div className="container cards">
+        <div className="max-w-5xl m-4 grid grid-cols-3 gap-4">
             {sorted.map(([year, total]) => (
-                <button title={`${year} hast du ${total / 100}€ ausgegeben`} className="glass card" onClick={() => onSelect(year)} key={year}>
-                    <h1>{year}</h1>
-                    <p className="balance">{total / 100}&euro;</p>
+                <button
+                    key={year}
+                    title={`${year} hast du ${formatCurrency(total / 100)} ausgegeben`}
+                    onClick={() => onSelect(year)}
+                    className="w-full flex flex-col text-left gap-3 justify-between px-5 py-4 rounded-2xl bg-white/5 border border-white/10 hover:bg-white/10 hover:border-indigo-400/40 transition"
+                >
+                    <span className="text-white/90 font-semibold text-4xl">{year}</span>
+                    <span className="text-indigo-400 font-medium">
+                        {formatCurrency(total/ 100)}
+                    </span>
                 </button>
             ))}
         </div>
