6e2b20aea6b8fe2f4e28e9f9c812dde1e4d1578c
Yasemin.Yazoglu <yaseminyazoglu@gmail.com>
2026-08-20 23:51:51 +0200

    add margin on smaller devices

--- app/auth/update-password/page.tsx
+++ app/auth/update-password/page.tsx
@@ -16,7 +16,7 @@ export default function UpdatePasswordPage() {
 
     return (
         <div className="min-h-screen flex justify-center items-start pt-24">
-            <div className="rounded-2xl bg-white/5 border border-white/10 w-full max-w-md py-8 px-6">
+            <div className="rounded-2xl bg-white/5 border border-white/10 w-full max-w-md py-8 px-6 mx-4">
                 <div className="text-center">
                     <Link href="/">
                         <Image src={Logo} alt="Kassenzettel" className="w-64 mx-auto" />
